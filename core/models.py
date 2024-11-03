960c6b7c1e798548a17924f04d866d0af599dff9
CHIGODI <chiegody254@gmail.com>
2024-11-03 16:26:04 +0300

    Fix pycodestyle errors && add documentation

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
+"""This modules contains the base model for all models in the application"""
+
 from django.db import models
 
+
 class BaseModel(models.Model):
+    """This class contains the base model for all models in the application"""
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     id = models.AutoField(primary_key=True)
 
     class Meta:
+        """This models is abstract and should not be created in the database"""
         abstract = True
