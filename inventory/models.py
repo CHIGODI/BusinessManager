b1ce3c40ecfb80b48e17d5d56114a43b610ed193
CHIGODI <chiegody254@gmail.com>
2024-11-03 20:56:57 +0300

    Add first endpoint(view) to inventory app, handling product listing and creation

--- inventory/models.py
+++ inventory/models.py
@@ -1,12 +1,13 @@
 """This module contains the model for products"""
 
 from django.db import models
-from ..core.models import BaseModel
+from core.models import BaseModel
 
 
 class Product(BaseModel):
     """Model for products"""
-    name = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False,
+                            unique=True)
     buying_price = models.DecimalField(max_digits=6, default=0.0,
                                        decimal_places=2, null=False)
     selling_price = models.DecimalField(max_digits=6, default=0.0,
