e796454a978648756e19185fed4c20cc6a759abd
CHIGODI <chiegody254@gmail.com>
2024-11-03 16:17:32 +0300

    Add new fields to product model

--- inventory/models.py
+++ inventory/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from core.models import BaseModel
 
 class Product(BaseModel, models.Model):
-    name = models.CharField(max_length=255)
-    buying_price = models.DecimalField(max_digits=5, decimal_places=2)
-    selling_price = models.DecimalField(max_digits=5, decimal_places=2)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=255, null=False, blank=False)
+    buying_price = models.DecimalField(default=0.0, decimal_places=2, null=False)
+    selling_price = models.DecimalField(default=0.0, decimal_places=2, null=False)
+    manufacturer = models.TextField(max_length=255, null=True, blank=True)
+    category = models.TextField(max_length=255, null=False, blank=False)
+    quantity = models.IntegerField(null=False, default=0)
 
     class Meta:
         db_table = 'products'
