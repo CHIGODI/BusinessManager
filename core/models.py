b17b9d3f426807e3df0d8d78226aa35afd8ccc42
CHIGODI <chiegody254@gmail.com>
2024-11-03 20:55:10 +0300

    make attribute id a UUID with uuid default value

--- core/models.py
+++ core/models.py
@@ -1,13 +1,16 @@
 """This modules contains the base model for all models in the application"""
 
 from django.db import models
+import uuid
 
 
 class BaseModel(models.Model):
     """This class contains the base model for all models in the application"""
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
 
     class Meta:
         """This models is abstract and should not be created in the database"""
