3f8fea3de9d9412119be170a5e56e66dba36331a
CHIGODI <chiegody254@gmail.com>
2024-11-03 18:52:35 +0300

    Add sale model, refine product model

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,7 @@
 """This module contains the model for products"""
 
 from django.db import models
-from core.models import BaseModel
+from ..core.models import BaseModel
 
 
 class Product(BaseModel):
@@ -12,8 +12,21 @@ class Product(BaseModel):
     selling_price = models.DecimalField(max_digits=6, default=0.0,
                                         decimal_places=2, null=False)
     manufacturer = models.TextField(max_length=255, null=True)
-    industry = models.TextField(max_length=255, null=False,
-                                default='Uncategorized')
+    industry_choices = [
+        ('Agriculture', 'Agriculture'),
+        ('Manufacturing', 'Manufacturing'),
+        ('Technology', 'Technology'),
+        ('Health', 'Health'),
+        ('Food', 'Food'),
+        ('Fashion', 'Fashion'),
+        ('Construction', 'Construction'),
+        ('Transport', 'Transport'),
+        ('Uncategorized', 'Uncategorized')]
+
+    industry = models.TextField(max_length=255,
+                                choices=industry_choices,
+                                default='Uncategorized',
+                                null=False)
     quantity = models.IntegerField(null=False, default=0)
 
     class Meta:
@@ -23,3 +36,22 @@ class Product(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Sale(BaseModel):
+    """Model for sales"""
+    product = models.ForeignKey(Product, on_delete=models.PROTECT,
+                                related_name='sales')
+    quantity = models.IntegerField(null=False, default=0)
+    discount = models.DecimalField(max_digits=6, default=0.0,
+                                   decimal_places=2, null=True)
+    total_cost = models.DecimalField(max_digits=6, default=0.0,
+                                     decimal_places=2, null=False)
+
+    class Meta:
+        """Control behavior of the model"""
+        db_table = 'sales'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.product} {self.number_products_sold}'
