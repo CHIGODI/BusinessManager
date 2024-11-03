31cad31babb052b57aa96e43ab73293c8f312893
CHIGODI <chiegody254@gmail.com>
2024-11-03 17:07:41 +0300

    Add max digits for b.p and s.p , add edit filed category to industry

--- inventory/models.py
+++ inventory/models.py
@@ -7,12 +7,13 @@ from core.models import BaseModel
 class Product(BaseModel):
     """Model for products"""
     name = models.CharField(max_length=255, null=False, blank=False)
-    buying_price = models.DecimalField(default=0.0, decimal_places=2,
-                                       null=False)
-    selling_price = models.DecimalField(default=0.0, decimal_places=2,
-                                        null=False)
+    buying_price = models.DecimalField(max_digits=6, default=0.0,
+                                       decimal_places=2, null=False)
+    selling_price = models.DecimalField(max_digits=6, default=0.0,
+                                        decimal_places=2, null=False)
     manufacturer = models.TextField(max_length=255, null=True)
-    category = models.TextField(max_length=255, null=False)
+    industry = models.TextField(max_length=255, null=False,
+                                default='Uncategorized')
     quantity = models.IntegerField(null=False, default=0)
 
     class Meta:
