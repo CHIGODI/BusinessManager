8882f1b20a64643736793868c117c61935c87e94
CHIGODI <chiegody254@gmail.com>
2024-11-30 09:34:54 +0300

    Delete BACKEND

--- BACKEND/inventory/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Product, Sale
-
-# Registering the models with the admin site
-admin.site.register(Product)
-admin.site.register(Sale)
--- BACKEND/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-
-# Registering the models with the admin site
-admin.site.register(CustomUser)
