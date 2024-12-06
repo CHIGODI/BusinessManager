375f9e9a3b0c5b9777106aef8b15ef4e202db24d
CHIGODI <chiegody254@gmail.com>
2024-12-06 15:11:00 +0300

    Add login with jwt views , with their respective urls

--- mybiz-backend/accounts/urls.py
+++ mybiz-backend/accounts/urls.py
@@ -4,11 +4,12 @@
 This module contains the URL configuration for the inventory app.
 """
 from django.urls import path
-from .views import RegisterView, LoginView, UserList
+from .views import RegisterView, LoginView, UserList, LogoutView
 
 
 urlpatterns = [
     path('account/register/', RegisterView.as_view(), name='register'),
     path('account/login/', LoginView.as_view(), name='login'),
+    path('account/logout/', LogoutView.as_view(), name='logout'),
     path('account/users/', UserList.as_view(), name='users'),
 ]
--- mybiz-backend/accounts/views.py
+++ mybiz-backend/accounts/views.py
@@ -1,15 +1,20 @@
+import jwt
+from os import getenv
+from dotenv import load_dotenv
 from .serializers import UserSerializer
-from rest_framework import generics
-from django.contrib.auth import get_user_model
-from rest_framework.exceptions import ValidationError
-from rest_framework.response import Response
 from rest_framework import generics, status
-import jwt, datetime
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from datetime import datetime, timedelta, timezone
+from rest_framework.exceptions import ValidationError
 
 
+load_dotenv()
 CustomUser = get_user_model()
 
+
 class RegisterView(generics.GenericAPIView):
+    """Register a new user/Create a new User"""
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
 
@@ -22,27 +27,53 @@ class RegisterView(generics.GenericAPIView):
         user = CustomUser(email=email)
         user.set_password(password)
         user.save()
-        return Response(self.serializer_class(user).data, status=status.HTTP_201_CREATED)
+        return Response(self.serializer_class(user).data,
+                        status=status.HTTP_201_CREATED)
+
 
 class LoginView(generics.GenericAPIView):
+    """Logs in a user"""
     serializer_class = UserSerializer
+    queryset = CustomUser.objects.all()
 
     def post(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
         user = CustomUser.objects.filter(email=email).first()
+        print(email, password)
+
+        if not email or not password:
+            raise ValidationError("Email and password are required")
         if user is None:
             raise ValidationError("User not found")
         if not user.check_password(password):
             raise ValidationError("Incorrect password")
+
+        issued_at = datetime.now(timezone.utc)
+        expiration_time = issued_at + timedelta(minutes=60)
+
         payload = {
             "id": user.id,
-            "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=60),
-            "iat": datetime.datetime.now(datetime.timezone.utc),
+            "exp": expiration_time.isoformat(),
+            "issued_at": issued_at.isoformat(),
         }
-        token = jwt.encode(payload, "secret", algorithm="HS256")
-        return Response({"token": token}, status=status.HTTP_200_OK)
+
+        token = jwt.encode(payload, getenv('SECRET'), algorithm="HS256")
+        res = Response({"message": "Success"})
+        res.set_cookie('jwt', token, httponly=True, secure=True)
+
+        return res
+
+
+class LogoutView(generics.GenericAPIView):
+    """Deletes the JWT"""
+    def post(self, request):
+        res = Response({"message": "Success"})
+        res.delete_cookie('jwt')
+        return res
+
 
 class UserList(generics.ListCreateAPIView):
+    """List all users"""
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
