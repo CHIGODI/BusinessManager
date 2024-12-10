0581561e98cfc407324a47609a10566192b87693
CHIGODI <chiegody254@gmail.com>
2024-12-10 15:54:25 +0300

    import logic function to perform logout and include path

--- mybiz-backend/accounts/urls.py
+++ mybiz-backend/accounts/urls.py
@@ -4,10 +4,10 @@
 This module contains the URL configuration for the inventory app.
 """
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LogoutView
 
 
 urlpatterns = [
     path('account/register/', RegisterView.as_view(), name='register'),
-    path('account/logout/', RegisterView.as_view(), name='logout'),
+    path('account/logout/', LogoutView.as_view(), name='logout'),
 ]
