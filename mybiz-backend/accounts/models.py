27a762e9ab9a423dd88216da346d60908906c44d
CHIGODI <chiegody254@gmail.com>
2024-12-10 14:10:54 +0300

    Apply pycodestyle fixes to improve code readability

--- mybiz-backend/accounts/models.py
+++ mybiz-backend/accounts/models.py
@@ -1,20 +1,28 @@
-from django.contrib.auth.models import AbstractUser
+"""
+This module contains the model that represents the
+user in the application
+"""
+
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 from core.models import BaseModel as BaseModel
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser, BaseModel):
+    """User model for the application"""
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(_("email address"), unique=True)
-
     objects = CustomUserManager()
 
     class Meta:
+        """Meta class for the user model"""
         db_table = "users"
         verbose_name = _("user")
         verbose_name_plural = _("users")
 
     def __str__(self):
-        return self.email
+        """Return the string representation of the user"""
+        return 'This is user {} with email {}'.format(self.username,
+                                                      self.email)
