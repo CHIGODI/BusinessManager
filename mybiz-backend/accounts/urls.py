1c547675347b6bd03f90ac4ca6b0d9bf0246ee6c
CHIGODI <chiegody254@gmail.com>
2024-12-10 14:12:45 +0300

    Delete all urls except register url for user creation

--- mybiz-backend/accounts/urls.py
+++ mybiz-backend/accounts/urls.py
@@ -4,12 +4,9 @@
 This module contains the URL configuration for the inventory app.
 """
 from django.urls import path
-from .views import RegisterView, LoginView, UserList, LogoutView
+from .views import RegisterView
 
 
 urlpatterns = [
     path('account/register/', RegisterView.as_view(), name='register'),
-    path('account/login/', LoginView.as_view(), name='login'),
-    path('account/logout/', LogoutView.as_view(), name='logout'),
-    path('account/users/', UserList.as_view(), name='users'),
 ]
