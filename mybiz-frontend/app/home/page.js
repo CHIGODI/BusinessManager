340eabb5396155e37e311a4be3647fd895c33fda
CHIGODI <chiegody254@gmail.com>
2024-12-10 21:00:57 +0300

    Change hero text message

--- mybiz-frontend/app/home/page.js
+++ mybiz-frontend/app/home/page.js
@@ -8,12 +8,14 @@ export default function LandingPage() {
             <NavBar />
             <section className="flex flex-row md:flex-row justify-between px-8 py-16 gap-10">
                 <div className="md:w-1/2 flex flex-col items-center mt-[8%] text-center">
-                    <h2 className="text-4xl font-bold text-[#001F3F] pb-4 ">
-                        Think, plan and track all in one place
-                    </h2>
-                    <p className="text-lg text-[#31363F] pb-4">
-                        Efficiently manage your business and boost productivity.
-                    </p>
+                    <header>
+                        <h2 className="text-4xl font-bold text-[#001F3F] pb-4 ">
+                            Welcome to myBIZ
+                        </h2>
+                        <p className="text-lg text-[#31363F] pb-4">
+                            Your one-stop solution for managing your business.
+                        </p>
+                    </header>
                     <button className="bg-purple-600 text-white px-3 py-3 text-lg rounded hover:bg-blue-700 w-1/2">
                         Get started
                     </button>
@@ -30,4 +32,4 @@ export default function LandingPage() {
             <Footer />
         </div>
     );
-}
+};
