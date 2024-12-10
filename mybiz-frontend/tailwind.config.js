5975ce3086fb0ba22af149be426889c5a35343c9
CHIGODI <chiegody254@gmail.com>
2024-12-10 10:29:57 +0300

    Add cutsom class styles i.e links, cards

--- mybiz-frontend/tailwind.config.js
+++ mybiz-frontend/tailwind.config.js
@@ -11,16 +11,19 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      boxShadow: {
+        'right-sm': '0.5px 0 1px 0 rgba(0, 0, 0, 0.05)',
+      },
     },
   },
   plugins: [
     function ({ addComponents }) {
       addComponents({
         '.links': {
-          '@apply text-[#001F3F] text-base w-[90%] ml-[2%] p-4 rounded-tr-full rounded-br-full hover:bg-[#F1EAFF] transition duration-300 ease-in-out': {},
+          '@apply text-[#001F3F] text-base w-[90%] ml-[2%] p-4 rounded-tr-full rounded-br-full hover:bg-[#F1EAFF] hover:text-purple-500 transition duration-300 ease-in-out': {},
         },
         '.cards': {
-          '@apply bg-white rounded-lg p-6 h-[40%] w-[30%] border': {},
+          '@apply bg-white rounded-lg p-6 h-[40%] w-[30%] border shadow-sm': {},
         },
       });
     },
