a28f501fcfb7f59978dbcc5aa6501ef2134cb389
CHIGODI <chiegody254@gmail.com>
2024-12-12 12:03:09 +0300

    Add user listing url

--- mybiz-backend/accounts/urls.py
+++ mybiz-backend/accounts/urls.py
@@ -4,10 +4,11 @@
 This module contains the URL configuration for the inventory app.
 """
 from django.urls import path
-from .views import RegisterView, LogoutView
+from .views import RegisterView, LogoutView, UserList
 
 
 urlpatterns = [
     path('account/register/', RegisterView.as_view(), name='register'),
     path('account/logout/', LogoutView.as_view(), name='logout'),
+    path('account/users/', UserList.as_view(), name='users')
 ]
