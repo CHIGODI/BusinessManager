4e96c7920823a8408e32be495c54d95e9bc68372
CHIGODI <chiegody254@gmail.com>
2024-12-13 19:31:55 +0300

    Add require scrolbar styles

--- mybiz-frontend/tailwind.config.js
+++ mybiz-frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwind-scrollbar');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,16 +16,18 @@ module.exports = {
       boxShadow: {
         'right-sm': '0.5px 0 1px 0 rgba(0, 0, 0, 0.05)',
       },
+      scrollbar: ['rounded'], // Move this to `extend` for styling scrollbar states
     },
   },
   plugins: [
+    require('tailwind-scrollbar'),
     function ({ addComponents }) {
       addComponents({
         '.links': {
-          '@apply text-[#001F3F] text-base w-[90%] ml-[2%] p-4 rounded-tr-full rounded-br-full hover:bg-[#F1EAFF] hover:text-purple-500 transition duration-300 ease-in-out': {},
+          '@apply text-[#001F3F] text-base w-[90%] ml-[2%] my-4 p-4 rounded-lg transition duration-300 ease-in-out': {},
         },
         '.cards': {
-          '@apply bg-white rounded-lg p-6 h-[40%] w-[100%] border shadow-sm md:w-[30%]': {},
+          '@apply bg-white rounded-lg p-6 h-[40%] w-[100%] border shadow-sm lg:w-[30%]': {},
         },
       });
     },
