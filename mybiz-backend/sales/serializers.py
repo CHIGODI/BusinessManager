b6ab553b778bc6b095779e99ecc89458b59a2368
Abenet Gebre <mail.micrq@proton.me>
2024-12-13 20:25:44 +0300

    sale POST, GET, and DELETE functionalities implemented

--- mybiz-backend/sales/serializers.py
+++ mybiz-backend/sales/serializers.py
@@ -6,3 +6,22 @@ class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = '__all__'
+
+    
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Quantity must be greater than 0")
+        return value
+    
+    def validate_discount(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                "Discount must be greater than or equal to 0")
+        return value
+    
+    def validate_total(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                "Total must be greater than or equal to 0")
+        return value
--- mybiz-backend/sales/views.py
+++ mybiz-backend/sales/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from .models import Sale
+from rest_framework import status
+from .serializers import SaleSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class SalesListCreate(APIView):
+    """ views for creating and listing sales
+        /api/v1/sales
+    """
+
+    def get(self, request):
+        """ get all sales """
+        sales = Sale.objects.all()
+        serializer = SaleSerializer(sales, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        """ create a sale """
+        serializer = SaleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(sold_by=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SaleDetail(APIView):
+    """ views for retrieving, updating and deleting a sale
+        /api/v1/sales/<int:id>
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, sale_id):
+        """ get a sale detail """
+        sale = get_object_or_404(Sale, pk=sale_id)
+        serializer = SaleSerializer(sale)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, sale_id):
+        """ delete a sale """
+        sale = get_object_or_404(Sale, pk=sale_id)
+        sale.delete()
+        return Response({"message": "Sale deleted successfully."},
+                        status=status.HTTP_204_NO_CONTENT)
