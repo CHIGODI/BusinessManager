0b5e952079d42cce72f33cd792c984c839094ebf
dianawanguikt048@gmail.com <chiegody254@gmail.com>
2025-07-08 12:54:37 +0300

    Remove unused Image import from LoginPage component

--- mybiz-frontend/app/login/page.js
+++ mybiz-frontend/app/login/page.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { toast } from "react-toastify";
-import Image from "next/image";
 
 
 const LoginPage = () => {
