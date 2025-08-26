5f9270720724c1b2e1d9b7a9a44af490827a38a9
dianawanguikt048@gmail.com <chiegody254@gmail.com>
2025-08-26 14:25:55 +0300

    Enhance ProductListCard: update search input styling for improved user experience

--- mybiz-frontend/app/user/dashboard/sale/components/ProductListCard.js
+++ mybiz-frontend/app/user/dashboard/sale/components/ProductListCard.js
@@ -23,7 +23,7 @@ const ProductListCard = ({products, addProductToCart}) => {
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
                             placeholder="Search..."
-                            className="text-xs w-full pl-10 pr-4 py-1 border-b border-gray-300 focus:outline-none focus:border-gray-600"
+                            className="bg-gray-100 text-xs w-full pl-10 pr-4 py-1 border border-gray-300 rounded-xl focus:outline-none focus:border-gray-400"
                         />
                         <FontAwesomeIcon
                             className="text-xs w-4 h-4 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2"
